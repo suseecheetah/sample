c300323231ce971ebdb79b18b60a08f207173efb
suseendiran <suseecheetah@gmail.com>
2020-03-20 15:52:28 +0530

    first package

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var Add = /** @class */ (function () {
+    function Add() {
+    }
+    Add.prototype.add = function (arg1, arg2) {
+        return arg1 + arg2;
+    };
+    return Add;
+}());
+exports.Add = Add;
---
+++ index.ts
@@ -0,0 +1,5 @@
+export class Add {
+    add(arg1: number, arg2: number) {
+        return arg1 + arg2
+    }
+}
